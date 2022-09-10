0dab2d9f2f154f375ff61574945090771cae1c00
Hoyeander <oyindamolayemisi@gmail.com>
2022-09-09 23:14:01 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: always returns 0
+ */
+int main(void)
+{
+	int i;
+i = 122;
+	while (i >= 97)
+	{								putchar(i);
+		i--;						}
+	putchar(10);
+	return (0);
+}

0a16d98b7bce221275ebb092c23c2a1588bd02b1
Hoyeander <oyindamolayemisi@gmail.com>
2022-09-13 14:38:49 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,14 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)
+	for (i = 0; i < 10; i++)
 	{
+		int j;
+
+		for (j = 97; j <= 122; j++)
+		{
 		_putchar(i);
+		}
+		_putchar(10);
 	}
-	_putchar(10);
 }

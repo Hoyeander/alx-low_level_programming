e168d11702ae66a94979cb3c92d3958a069f6dfd
Hoyeander <oyindamolayemisi@gmail.com>
2022-10-05 09:50:08 -0700

    Task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,4 +8,22 @@
  *
  */
 
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int b = 0;
+	int c = 0;
+	int s = size * size;
 
+	for (i = 0; i < s; i += size + 1)
+	{
+		b += a[i];
+	}
+
+	for (i = size - 1; i < s - 1; i += size - 1)
+	{
+		c += a[i];
+	}
+
+	printf("%d, %d\n", b, c);
+}

152be81611612f5047896e5266a50dfaf1368c99
Hoyeander <oyindamolayemisi@gmail.com>
2022-10-20 15:03:49 -0700

    task linked list

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: value of the converted string
+ */
+int _atoi(char *s)
+{
+	int a = 1;
+	unsigned int n = 0;
+
+	do {
+		if (*s == '-')
+			a *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+
+		else if (n > 0)
+			break;
+
+	} while (*s++);
+
+	return (n * a);
+}

626cb3634f313670d3e7e3661735c625e8d37e8c
Hoyeander <oyindamolayemisi@gmail.com>
2022-11-11 08:33:42 -0800

    Task completion

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *main - entry point
+ * main - Prints numbers between 0 to 9 and letters between a to f.
  *
- * Return: always return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		int i;
+	int i;
 
-			char jj;
-for (i = 0; i < 10; i++)
-				putchar((i % 10) + '0');
-			for (jj = 'a', jj <= 'f', jj++)
-								putchar(jj);
-						putchar(10);
-							return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }

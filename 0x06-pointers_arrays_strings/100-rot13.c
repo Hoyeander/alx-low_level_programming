846ca27d54090417dc7291cf9612e6fe7806b9fb
Hoyeander <oyindamolayemisi@gmail.com>
2022-12-15 03:01:56 -0800

    Task Adv

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,35 +1,28 @@
 #include "main.h"
-
 /**
- * rot13 - Encodes a string using rot13.
- * @n: The string to be encoded.
- * Return: A pointer to the encoded string.
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
-char *rot13(char *n)
-{
-		int index_x1 = 0, index_x2;
-			char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-							     'G', 'H', 'I', 'J', 'K', 'L',
-							     			     'M', 'N', 'O', 'P', 'Q', 'R',
-										     			     'S', 'T', 'U', 'V', 'W', 'X',
-													     			     'Y', 'Z', 'a', 'b', 'c', 'd',
-																     			     'e', 'f', 'g', 'h', 'i', 'j',
-																			     			     'k', 'l', 'm', 'n', 'o', 'p',
-																						     			     'q', 'r', 's', 't', 'u', 'v',
-																									     			     'w', 'x', 'y', 'z'};
-				char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-								     'T', 'U', 'V', 'W', 'X', 'Y',
-								     			     'Z', 'A', 'B', 'C', 'D', 'E',
-											     			     'F', 'G', 'H', 'I', 'J', 'K',
-														     			     'L', 'M', 'n', 'o', 'p', 'q',
-																	     			     'r', 's', 't', 'u', 'v', 'w',
-																				     			     'x', 'y', 'z', 'a', 'b', 'c',
-																							     			     'd', 'e', 'f', 'g', 'h', 'i',
-																										     			     'j', 'k', 'l', 'm'};
 
-					while (n[index_x1])
-							{
-										for (index_x2 = 0; index_x2 < 52; index_x2++)
-													{
-																	if (n[index_x1] == alphabet[index_x2]){n[index_x1] = rot13key[index_x2];break;}index_x1++;}return (n);
+char *rot13(char *s)
+{
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
 }
